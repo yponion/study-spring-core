dfec22fb5b22195ec0d476e8adf180bcb7f1c1ea
양정운 <yp071704@gmail.com>
2026-02-07 21:43:23 +0900

    feat: [2-4-25] 스프링으로 전환하기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,13 @@ package hello.core;
 import hello.core.member.Gradle;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Gradle.VIP);
         memberService.join(member);
 
