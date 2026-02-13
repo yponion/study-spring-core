bdeb67f86313e67639068bae7c04ea1eb990c441
양정운 <yp071704@gmail.com>
2026-02-13 23:21:28 +0900

    feat: [2-10-63] 스코프와 Provider

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+        myLogger.setRequestURL(requestURL); //  컨트롤러 보다는 공통 처리가 가능한 스프링 인터셉터나 서블릿 필터 같은 곳을 활용하는 것이 좋다
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
